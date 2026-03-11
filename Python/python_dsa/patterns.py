f8623116f5f587ee8e18ffe64d4d3c0d58b76e02
harinetha <harinetha@gmail.com>
2026-03-11 15:10:28 +0530

    march 11 completed work

--- Python/python_dsa/patterns.py
+++ Python/python_dsa/patterns.py
@@ -98,8 +98,112 @@
 #         print(j, end=" ")
 #     print()
 
+# n=5
+# for i in range(1,n+1):
+#     for j in range(n-i-1):
+#         print(j,end=' ')
+#     print()
+
+# PYRAMID PATTERN
 n=5
-for i in range(1,n+1):
-    for j in range(n-i-1):
-        print(j,end=' ')
+for i in range(1, n+1):
+        print(" "*(n-i) + "*" *(2*i-1))
+
+# RIGHT PYRAMID PATTERN
+n=5
+for i in range(n, 0, -1):
+        print(" "*(n-i) + "*" *(2*i-1))
+print()
+# # DIAMOND PATTERN
+# n=5
+# for i in range(1, n+1):
+#         print(" "*(n-i) + "*" *(2*i-1))
+
+# for i in range(n, 0, -1):
+#         print(" "*(n-i) + "*" *(2*i-1))
+
+# HOLLOW SQUARE PATTERN
+n=4
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
     print()
+
+
+# FLOYD'S TRIANGLE
+n=5
+num=1
+for i in range(1, n+1):
+    for j in range(i):
+        print(num, end=" ")
+        num+=1
+    print()
+print()
+
+
+# NUMBER PYRAMID
+n=5
+for i in range(1, n+1):
+    print(" "*(n-i), end="")
+    for j in range(1, i+1):
+        print(j, end="")
+    for j in range(i-1, 0, -1):
+        print(j, end="")
+    print()
+
+
+# *
+# * *
+# * * *
+# * * * *
+# * * *
+# * * 
+# *
+n=5
+for i in range(1, n+1):
+    print("* " * i)
+for i in range(n-1, 0, -1):
+    print("* " * i)
+
+
+# 1
+# 22
+# 333
+# 4444
+n=5
+for i in range(1, n+1):
+    print(str(i) * i) #
+
+
+
+# A
+# A B A 
+# A B C B A 
+# A B C D C B A
+n=5
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(chr(64 + j), end="")
+    for j in range(i-1, 0, -1):
+        print(chr(64 + j), end="")
+    print()
+
+
+
+# *
+# * *
+# *   *
+# *     *
+# * * * * *
+n=5
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        if j==1 or j==i or i==n:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
